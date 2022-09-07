bc971dbe7fb367916e3163e36208f3b7b09e426a
Christopher Wahlström <83230387+christopherwahlstrom@users.noreply.github.com>
2022-09-07 14:33:33 +0200

    Added footer in about and Adoption.tsx

--- src/Views/Adoption.tsx
+++ src/Views/Adoption.tsx
@@ -1,10 +1,11 @@
 import Header from "../components/Header";
-
+import Footer from "../components/Footer";
 const Adoption = () =>{
     return(
         <>
         <Header/>
         <h1>adoption</h1>
+        <Footer />
         </>
     )
 }
