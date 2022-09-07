91e93678ce967f6275b621fb327560427f3343c0
Christopher Wahlström <83230387+christopherwahlstrom@users.noreply.github.com>
2022-09-07 14:29:41 +0200

    Footer
    addat , scss samt tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,12 +9,18 @@ const Footer = () => {
     return (
         <section className="footer">
             <article>
-                
-
+                <p className='headline'>Rescue rabbits</p>
+                <p>© 2022</p>
+                <p>Privacy Terms</p>
             </article>
-
             <article>
-                <ul className='company__links'>
+                <a href="#" className="fa fa-facebook"></a>
+                <a href="#" className="fa fa-twitter"></a>
+                <a href="#" className="fa fa-instagram"></a>
+            </article>
+            <article>
+                <ul>
+                    <p className='headline'>Företag</p>
                     <li onClick={() => navigate('/About')}><a>Om oss</a></li>
                     <li onClick={() => navigate('/Adoption')}><a>Att Adoptera</a></li>
                 </ul>
