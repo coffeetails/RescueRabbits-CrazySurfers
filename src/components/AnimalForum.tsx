82e12153ffb9301f626c884b1a6ce2cc75c0099c
Zeeroon-e <chia.kmohamad@hotmail.com>
2022-09-13 09:57:40 +0200

    forms fixed

--- src/components/AnimalForum.tsx
+++ src/components/AnimalForum.tsx
@@ -22,18 +22,18 @@ const AnimalForum = () => {
     return(
 
         <section className="forum-container">
-            <form onSubmit={submitForm}>
+            <form className='animal-form' onSubmit={submitForm}>
                 <h1>Intresseanmälan</h1>
-                <label htmlFor="fullName">För och efternamn *</label>
-                <input type="text" id='fullName'   value={fullName} onChange={(e) => setFullName(e.target.value)} required/>
-                <label htmlFor="phone">Telefonnummer *</label>
-                <input type="text" id='phone' name='phone' required maxLength={10} minLength={10}/>
-                <label htmlFor="email">E-post *</label>
-                <input type="email" id='email' name='email' required  placeholder='exempel@hotmail.com'/>
-                <label htmlFor="currentAnimalInfo">Nuvarande djur i hemmet</label>
-                <textarea name="" id="currentAnimalInfo" cols={30} rows={15} ></textarea>
-                <label htmlFor="currentHomeInfo">Hem och familjesituation</label>
-                <textarea name="" id="currentHomeInfo" cols={30} rows={15}></textarea>
+                <label className='frm-label' htmlFor="fullName">För och efternamn *</label>
+                <input className='frm-input' type="text" id='fullName'   value={fullName} onChange={(e) => setFullName(e.target.value)} required placeholder='Adam Svensson'/>
+                <label  className='frm-label' htmlFor="phone">Telefonnummer *</label>
+                <input className='frm-input' type="text" id='phone' name='phone' required maxLength={10} minLength={10} placeholder='0788228822'/>
+                <label className='frm-label' htmlFor="email">E-post *</label>
+                <input className='frm-input' type="email" id='email' name='email' required  placeholder='exempel@hotmail.com'/>
+                <label className='frm-label' htmlFor="currentAnimalInfo">Nuvarande djur i hemmet</label>
+                <textarea className='frm-textarea' name="" id="currentAnimalInfo" cols={30} rows={15} ></textarea>
+                <label className='frm-label' htmlFor="currentHomeInfo">Hem och familjesituation</label>
+                <textarea className='frm-textarea' name="" id="currentHomeInfo" cols={30} rows={15}></textarea>
                 <button className='submit-btn'>Ansök</button>
             </form>
             {openModal && <Modal username={fullName} closeModal={setOpenModal}/>}
